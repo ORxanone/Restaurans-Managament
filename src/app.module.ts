8152a37e57a13c221138f31609a08aa940dfa888
ORxanone <recebovorxan3@gmail.com>
2025-01-12 16:44:03 +0400

    docker compose add db-gate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseModule } from 'database';
 import { ConfigModule } from '@nestjs/config';
 import { configuration } from 'config';
@@ -16,17 +15,6 @@ import {
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: 'root',
-    //   database: 'restaurant',
-    //   entities: [`${__dirname}/**/*.entity{.ts,.js}`],
-    //   migrations: [`${__dirname}/migrations/*{.ts,.js}`],
-    //   synchronize: false,
-    // }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
